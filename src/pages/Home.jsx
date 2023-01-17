c2e05083420ceb2d7e8c981a3c6f51a510265170
Sepidehmnt <sepidehmontazer@hotmail.com>
2023-01-17 14:49:40 +0330

    Add Download section in resturant's website

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import PopularMenu from "../components/popular-menu/PopularMenu";
 import ChooseUs from "../components/choose-us/ChooseUs";
 import MenuPack from "../components/menu-pack/MenuPack";
 import Testimonials from "../components/testimonials/Testimonials";
+import Download from "../components/download-section/Download";
 const Home = () => {
   return (
     <Fragment>
@@ -15,6 +16,7 @@ const Home = () => {
       <ChooseUs />
       <MenuPack />
       <Testimonials />
+      <Download />
     </Fragment>
   );
 };
